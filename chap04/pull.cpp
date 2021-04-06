548a7db9beabe2a667e72e72f1a96d33b80b9c29
tmknom <tmk.nom@gmail.com>
2021-04-06 13:28:48 +0900

    pullの実装

---
+++ chap04/pull.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template<class T>
+void chmin(T &a, T b) {
+    if (a > b) a = b;
+}
+
+const long long INF = 1LL << 60;
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<int> h(N);
+    for (int i = 0; i < N; ++i) cin >> h[i];
+
+    vector<long long> dp(N, INF);
+
+    dp[0] = 0;
+
+    for (int i = 1; i < N; ++i) {
+        chmin(dp[i], dp[i - 1] + abs(h[i] - h[i - 1]));
+        if (i > 1) {
+            chmin(dp[i], dp[i - 2] + abs(h[i] - h[i - 2]));
+        }
+        cout << "dp[" << i << "] = " << dp[i] << endl;
+    }
+
+    cout << dp[N - 1] << endl;
+}

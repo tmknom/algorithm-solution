a79f17e2bc6de1b7d742405095258900086469ff
tmknom <tmk.nom@gmail.com>
2021-04-06 10:27:35 +0900

    euclidの実装

---
+++ chap04/euclid.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int GCD(int m, int n) {
+    if (n == 0) return m;
+    cout << "GCD(" << m << " mod " << n << ") = " << (m % n) << endl;
+    return GCD(n, m % n);
+}
+
+int main() {
+    cout << GCD(51, 15) << endl;
+    cout << endl;
+    cout << GCD(15, 51) << endl;
+}
